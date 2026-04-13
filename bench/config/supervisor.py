aad7877798f0d18331227d37742d2f901fc5b257
nikkovanessa <nikkovaness@gmail.com>
2026-04-13 15:12:14 +0700

    update: add limit_request_line supervisor config

--- bench/config/supervisor.py
+++ bench/config/supervisor.py
@@ -60,6 +60,7 @@ def generate_supervisor_config(bench_path, user=None, yes=False, skip_redis=Fals
 			"workers": config.get("workers", {}),
 			"multi_queue_consumption": can_enable_multi_queue_consumption(bench_path),
 			"supervisor_startretries": 10,
+			"limit_request_line": config.get("limit_request_line"),
 		}
 	)
 
